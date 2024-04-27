3719840c3f79b0abe3b5c3e1000958d4ef8c62d2
Tahir <122523449+thrhead@users.noreply.github.com>
2024-04-27 10:53:42 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+'use strict';
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+const displayNumber = function (number) {
+  document.querySelector('.number').textContent = number;
+};
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  if (!guess) {
+    displayMessage('⛔️ No number!');
+  } else if (guess === secretNumber) {
+    displayMessage('🏆 Yeah, Correct Number!');
+    displayNumber(secretNumber);
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+
+    // when guess is wrong
+  } else if (guess !== secretNumber) {
+    displayMessage(guess > secretNumber ? '⬇️ Too High! ' : ' ⬆️ Too Low! ');
+  }
+});
+//again part
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+  displayMessage('Start guessing...');
+
+  document.querySelector('.score').textContent = score;
+  displayNumber('?');
+
+  document.querySelector('.guess').value = '';
+
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+});
